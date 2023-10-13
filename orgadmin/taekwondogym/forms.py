b09291c2c393a9428dea3328c6de8f901498110b
Khemmapop Boonploy <2boonploy@gmail.com>
2023-10-13 22:19:09 +0700

    update

---
+++ orgadmin/taekwondogym/forms.py
@@ -0,0 +1,87 @@
+from django import forms
+from .models import Member,Staff,Classes,Course,MemberAttendance,StaffAttendance,Invoice,Payroll
+
+MEMBER_STATUS = (
+    ('Active','Active'),
+    ('Disabled','Disabled')
+)
+
+COURSE_TYPE = (
+    ('SINGLE','SINGLE'),
+    ('CONTINUE','CONTINUE')
+)
+
+INVOICE_STATUS = (
+    ('PAID','PAID'),
+    ('HOLD','HOLD'),
+    ('FAILED','FAILED'),
+    ('CANCEL','CANCEL')
+)
+
+PAYMENT_TYPE = (
+    ('ONLINE','ONLINE'),
+    ('PROMPTPAY','PROMPTPAY'),
+    ('CASH','CASH')
+)
+
+class MemberAddForm(forms.ModelForm):
+    class Meta:
+        model = Member
+        fields = ['profile_pic','name','address','parent_name','email_id','phone','line_id','member_status']
+        
+class MemberUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Member
+        fields = ['profile_pic','name','address','parent_name','email_id','phone','line_id','member_status','leave_at','point']
+        
+class StaffAddForm(forms.ModelForm):
+    
+    class Meta:
+        model = Staff
+        fields = ['profile_pic','name','address','email_id','phone','line_id','member_status','leave_at']
+        
+class StaffUpdateForm(forms.ModelForm):
+    
+    class Meta:
+        model = Staff
+        fields = ['profile_pic','name','address','email_id','phone','line_id','member_status','leave_at']
+        
+class ClassAddForm(forms.ModelForm):
+    class Meta:
+        model = Classes
+        fields = ['name','level_name','students']
+
+class ClassUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Classes
+        fields = ['name','level_name','students','update_at']
+        
+class CourseAddForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ['course_name','course_type','course_fee','staff','open_course','close_course','classes']
+        
+class CourseUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ['course_name','course_type','course_fee','staff','open_course','close_course','classes']
+        
+class InvoiceAddForm(forms.ModelForm):
+    class Meta:
+        model = Invoice
+        fields = ['invoice_number','students','invoice_status','courses','amount','discount','pay','remark','payment_type']
+        
+class InvoiceUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Invoice
+        fields = ['invoice_number','students','invoice_status','courses','amount','discount','pay','remark','payment_type']
+
+class PayrollAddForm(forms.ModelForm):
+    class Meta:
+        model = Payroll
+        fields = ['staff','amount','bonus','pay','hour_amount','pay_at']
+        
+class PayrollUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Payroll
+        fields = ['staff','amount','bonus','pay','hour_amount','pay_at']
---
+++ orgadmin/taekwondogym/urls.py
@@ -0,0 +1,59 @@
+from django.urls import path
+from taekwondogym.views import(
+    member_add_view,
+    member_update_view,
+    member_list_view,
+
+    staff_add_view,
+    staff_update_view,
+    staff_list_view,
+
+    class_add_view,
+    class_update_view,
+    class_list_view,
+
+    course_add_view,
+    course_update_view,
+    course_list_view,
+
+    invoice_add_view,
+    invoice_update_view,
+    invoice_list_view,
+
+    payroll_add_view,
+    payroll_update_view,
+    payroll_list_view,
+
+    member_attendance_view,
+    staff_attendance_view
+)
+app_name = "taekwondogym"
+
+urlpatterns = [
+    path("add-member", view=member_add_view, name="member.add"),
+    path("update-member", view=member_update_view, name="member.update"),
+    path("list-member", view=member_list_view, name="member.list"),
+
+    path("add-staff", view=staff_add_view, name="staff.add"),
+    path("update-staff", view=staff_update_view, name="staff.update"),
+     path("list-staff", view=staff_update_view, name="staff.list"),
+
+    path("add-class", view=class_add_view, name="class.add"),
+    path("update-class", view=class_update_view, name="class.update"),
+    path("list-class", view=class_update_view, name="class.list"),
+
+    path("add-course", view=course_add_view, name="course.add"),
+    path("update-course", view=course_update_view, name="course.update"),
+    path("list-course", view=course_list_view, name="course.list"),
+
+    path("add-invoice", view=invoice_add_view, name="invoice.add"),
+    path("update-invoice", view=invoice_update_view, name="invoice.update"),
+    path("list-invoice", view=invoice_update_view, name="invoice.list"),
+
+    path("add-payroll", view=payroll_add_view, name="payroll.add"),
+    path("update-payroll", view=payroll_update_view, name="payroll.update"),
+    path("list-payroll", view=payroll_update_view, name="payroll.list"),
+
+    path("member-attendance", view=member_attendance_view, name="member.attendance"),
+    path("staff-attendance", view=staff_attendance_view, name="staff.attendance"),
+]
